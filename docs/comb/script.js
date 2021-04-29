8997c8b07a16ea65405a3b3116cd2713b257009d
Md. Arifuzzaman <arif69eee@gmail.com>
2021-04-29 19:55:55 +0600

    Update script.js

--- docs/comb/script.js
+++ docs/comb/script.js
@@ -4,9 +4,29 @@ $(document).ready(function()
 
     setTimeout(function() 
     {
-        alert($("#VRButton"));
-        alert($("#VRButton")[0]);
-
-        $("#VRButton")[0].click();
+        toggleThreejsVR();
     }, 5000);
 });
+
+
+function toggleThreejsVR()
+{
+    if ( currentSession === null ) {
+
+        // WebXR's requestReferenceSpace only works if the corresponding feature
+        // was requested at session creation time. For simplicity, just ask for
+        // the interesting ones as optional features, but be aware that the
+        // requestReferenceSpace call will fail if it turns out to be unavailable.
+        // ('local' is always available for immersive sessions and doesn't need to
+        // be requested separately.)
+
+        const sessionInit = { optionalFeatures: [ 'local-floor', 'bounded-floor', 'hand-tracking' ] };
+        navigator.xr.requestSession( 'immersive-vr', sessionInit ).then( onSessionStarted );
+
+    } else {
+
+        currentSession.end();
+
+    }
+
+}
