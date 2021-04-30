cb66cd6e79d38b7b222322166a4579e5979d8d8d
Md. Arifuzzaman <arif69eee@gmail.com>
2021-04-30 21:44:32 +0600

    Update script.js

--- docs/comb/script.js
+++ docs/comb/script.js
@@ -3,6 +3,8 @@ $(document).ready(function()
     //alert('$(document).ready(function()');
     //$("#VRButton").hide();
 
+    var lastSceneClosed = null;
+
     function trySwitchingThreejsUnityScene()
     {
         if(window.xrManager.isInVRSession)
@@ -20,16 +22,32 @@ $(document).ready(function()
     window.unityxr_onEndSession = function()
     {
         //alert('window.unityxr_onEndSession = function()');
-        setTimeout(function(){window.focus();}, 6000);
-        setTimeout(function(){window.threejsxr_vrbtn.onclick();}, 7000);
+        //setTimeout(function(){window.focus();}, 6000);
+        //setTimeout(function(){window.threejsxr_vrbtn.onclick();}, 7000);
+        lastSceneClosed = "unityxr";
+        $('#switch-scenes-btn').show();
     }
 
     window.threejsxr_onSessionEnded = function()
     {
         //alert('window.threejsxr_onSessionEnded = function()');
-        setTimeout(function(){window.focus();}, 6000);
-        setTimeout(function(){window.xrManager.toggleVR();}, 7000);
+        //setTimeout(function(){window.focus();}, 6000);
+        //setTimeout(function(){window.xrManager.toggleVR();}, 7000);
+        lastSceneClosed = "threejsxr";
+        $('#switch-scenes-btn').show();
     }
+
+    $('#switch-scenes-btn').click(function()
+    {
+        if(lastSceneClosed == "unityxr")
+        {
+            window.threejsxr_vrbtn.onclick();
+        }
+        else if(lastSceneClosed == "threejsxr")
+        {
+            window.xrManager.toggleVR();
+        }
+    });
 
     trySwitchingThreejsUnityScene();
 
