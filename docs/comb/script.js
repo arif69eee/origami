c60edfd2eccf5522995a87a9634d2172dbfa834f
Md. Arifuzzaman <arif69eee@gmail.com>
2021-04-30 17:20:58 +0600

    Update script.js

--- docs/comb/script.js
+++ docs/comb/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function()
 {
-    alert('$(document).ready(function()');
+    //alert('$(document).ready(function()');
     //$("#VRButton").hide();
-    setTimeout(function() {
-        window.threejsxr_vrbtn.onclick(); 
-      }, 3000);
+
+    function trySwitchingThreejsUnityScene()
+    {
+        if(window.xrManager.isInVRSession)
+        {
+            window.xrManager.toggleVR();
+            window.threejsxr_vrbtn.onclick();
+        }
+        else if(window.threejsxr_vrbtn.textContent == 'EXIT VR')
+        {
+            window.threejsxr_vrbtn.onclick();
+            window.xrManager.toggleVR();
+        }
+
+        setTimeout(trySwitchingThreejsUnityScene, 5000);        
+    }
+
+    trySwitchingThreejsUnityScene();
 });
-
-
-function toggleThreejsVR()
-{
-    
-}
