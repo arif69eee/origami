db60544aec0f18163f2cfaecc7ea891c3eefa172
Md. Arifuzzaman <arif69eee@gmail.com>
2021-04-30 19:37:00 +0600

    Update script.js

--- docs/comb/script.js
+++ docs/comb/script.js
@@ -19,14 +19,14 @@ $(document).ready(function()
 
     window.unityxr_onEndSession = function()
     {
-        alert('window.unityxr_onEndSession = function()');
+        //alert('window.unityxr_onEndSession = function()');
         $("#VRButton").focus();
         setTimeout(window.threejsxr_vrbtn.onclick, 7000);
     }
 
     window.threejsxr_onSessionEnded = function()
     {
-        alert('window.threejsxr_onSessionEnded = function()');
+        //alert('window.threejsxr_onSessionEnded = function()');
         $("#VRButton").focus();
         setTimeout(window.xrManager.toggleVR, 7000);
     }
