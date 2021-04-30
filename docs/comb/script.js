ca5cb58b28821fad315a79e48461970e63519053
Md. Arifuzzaman <arif69eee@gmail.com>
2021-04-30 16:26:14 +0600

    Update script.js

--- docs/comb/script.js
+++ docs/comb/script.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     //$("#VRButton").hide();
 
     setTimeout(function() {
-        //window.threejsxr_vrbtn.onclick(); 
+        window.threejsxr_vrbtn.onclick(); 
       }, 3000);
 });
 
